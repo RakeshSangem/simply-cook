5b478f26e6e155f7b2c31a37a3016d64f1cbe1bb
Rakesh Sangem <107752425+RakeshSangem@users.noreply.github.com>
2023-03-11 21:20:53 +0530

    feat: added next/font (#3)
    * fix: small changes
    
    * feat: add next/font
    
    ---------
    
    Co-authored-by: Rakesh Sangem <rakeshsangem8@gmail.com>

--- components/Home.jsx
+++ components/Home.jsx
@@ -26,6 +26,9 @@ export default function Home() {
           </Link>
         </div>
       </div>
+      <section className="">
+        <div>Find your favorite recipe at your finger tips</div>
+      </section>
     </main>
   );
 }
