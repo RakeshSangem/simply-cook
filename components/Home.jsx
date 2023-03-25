a281c1f8c8612a408322f26b9f4aaf9210511fe2
Rakesh Sangem <107752425+RakeshSangem@users.noreply.github.com>
2023-03-25 18:31:56 +0530

    Fix layout (#6)
    * fix: layout issues
    
    * fix: layout issues
    
    ---------
    
    Co-authored-by: Rakesh Sangem <rakeshsangem8@gmail.com>

--- components/Home.jsx
+++ components/Home.jsx
@@ -7,28 +7,30 @@ const nunito = Nunito({
 
 export default function Home() {
   return (
-    <main
-      className={`${nunito.className} container mx-auto grid place-items-center bg-cover bg-center px-4`}
-    >
-      <div className="mx-auto max-w-screen-lg py-12 text-center">
-        <h1 className="leading-tighter mb-4 max-w-3xl text-5xl font-bold tracking-tighter md:text-6xl">
-          Search over 2.3M+ <span>Recipes</span> from our app
-        </h1>
-        <p className="mx-auto w-3/4 text-2xl font-semibold text-black/40">
-          Discover recipes from our huge database and become better chef
-        </p>
-        <div className="py-10">
-          <Link
-            href="/recipe"
-            className="rounded-md bg-blue-400 px-8 py-3 text-xl font-medium text-white"
-          >
-            Explore Recipes
-          </Link>
+    <>
+      <main
+        className={`${nunito.className} z-3 relative mx-auto grid place-items-center bg-cover bg-center px-4`}
+      >
+        <div className=" py-12 text-center">
+          <h1 className="leading-tighter mb-4 max-w-3xl text-5xl font-bold tracking-tighter md:text-6xl">
+            Search over 2.3M+ <span>Recipes</span> from our app
+          </h1>
+          <p className="mx-auto w-3/4 text-2xl font-semibold text-black/40">
+            Discover recipes from our huge database and become better chef
+          </p>
+          <div className="py-10">
+            <Link
+              href="/recipe"
+              className="rounded-md bg-blue-400 px-8 py-3 text-xl font-medium text-white"
+            >
+              Explore Recipes
+            </Link>
+          </div>
         </div>
-      </div>
-      <section className="">
-        <div>Find your favorite recipe at your finger tips</div>
-      </section>
-    </main>
+        <section className="">
+          <div>Find your favorite recipe at your finger tips</div>
+        </section>
+      </main>
+    </>
   );
 }
