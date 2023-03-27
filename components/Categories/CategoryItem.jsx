f37f683ecddd4fe2fbce3b0b626cc472b4f29430
Rakesh Sangem <rakeshsangem8@gmail.com>
2023-03-27 11:07:43 +0530

    clean code

--- components/Categories/CategoryItem.jsx
+++ components/Categories/CategoryItem.jsx
@@ -8,11 +8,11 @@ export default function CategoryItem({ categoryData, onImageLoaded }) {
   const urlPath = url.pathname.split("/");
   const id = urlPath[urlPath.length - 1];
 
-  // console.log(id);
   return (
     <Link href={`/recipe/${id}`}>
       <article className="group relative w-80 max-w-sm snap-center overflow-hidden rounded-3xl border border-slate-300 bg-white shadow hover:shadow-xl">
         <div className="h-60">
+          {/* eslint-disable-next-line @next/next/no-img-element */}
           <img
             onLoad={() => onImageLoaded(false)}
             className="h-full w-full rounded-sm object-cover"
@@ -20,7 +20,7 @@ export default function CategoryItem({ categoryData, onImageLoaded }) {
             alt={label}
           />
         </div>
-        <div className="fade-up absolute bottom-0 flex h-1/2 w-full items-end justify-center pb-5 text-center px-4">
+        <div className="fade-up absolute bottom-0 flex h-1/2 w-full items-end justify-center px-4 pb-5 text-center">
           <h2 className="text-lg text-white">{label}</h2>
         </div>
       </article>
